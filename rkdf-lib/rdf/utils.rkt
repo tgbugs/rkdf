925392d47c6a4e6821d7a38819f21b0b97bb8bc9
Tom Gillespie <tgbugs@gmail.com>
2018-08-27 22:11:41 -0700

    rdf/utils match-triples with transitive, qnames and namespaces

--- rkdf-lib/rdf/utils.rkt
+++ rkdf-lib/rdf/utils.rkt
@@ -1,11 +1,19 @@
 #lang racket/base
 (require racket/generic
-         racket/string)
+         racket/list
+         racket/hash
+         racket/string
+         (for-syntax racket/base
+                     racket/string
+                     racket/syntax
+                     syntax/parse))
 (provide (all-defined-out)
          (struct-out Literal)
          (struct-out triple)
          (struct-out BNode)
          (struct-out URI))
+(module+ test
+  (require rackunit (for-syntax rackunit)))
 
 (define (bracket uri)
   (string-append "<" uri ">"))
@@ -14,11 +22,17 @@
   ; TODO consider using current output port to do this directly
   (gen-ser serializable)
   (gen-value serializable)
+  (gen-short serializable)
   )
+
+(define iri-prefixes (make-parameter (make-hash)))
+
+;;; internal representation of entities
 
 (struct URI (value) #:inspector #f
   #:methods gen:serializable
   [(define (gen-value uri) (URI-value uri))
+   (define (gen-short uri) (qname (URI-value uri)))
    (define (gen-ser uri)
      (string-append (bracket (URI-value uri))))])
 
@@ -26,8 +40,7 @@
   #:methods gen:serializable
   ; TODO
   [(define (gen-ser trip)
-     trip)
-   ])
+     trip)])
 
 (struct Literal (value type lang) #:inspector #f
   #:methods gen:serializable
@@ -57,3 +70,208 @@
    (define (gen-ser bnode)
      (string-append "_:" (BNode-value bnode)))])
 
+;;; store
+
+(define (instrument-store [store '()])
+  (define int-triples store)
+
+  (define (add-triple triple)
+    (set! int-triples (cons triple int-triples)))
+
+  (define (add-triples triples)
+    (set! int-triples (append triples int-triples)))
+
+  (define-syntax (null?-or-equal? stx) 
+    (syntax-parse stx
+      [(_ nullable to-match)
+       #'(or (null? nullable) (equal? nullable to-match))]))
+
+  (define (-match-triples #:s [s null] #:p [p null] #:o [o null])
+    (define (f s-t p-t o-t)
+      (and (null?-or-equal? s s-t)
+           (null?-or-equal? p p-t)
+           (null?-or-equal? o o-t)
+           ))
+    (filter (λ (t) (apply f t)) int-triples))
+
+  (define (match-triples #:s [s null] #:p [p null] #:o [o null] #:transitive [transitive #f])
+    "if #:transitive is set the a value given to #:s or to #:o is interpreted as the starting point"
+    (define (f t)
+      (and (null?-or-equal? s (gen-value (triple-s t)))
+           (null?-or-equal? p (gen-value (triple-p t)))
+           (null?-or-equal? o (gen-value (triple-o t)))))
+    (if transitive
+        ; get the first set of result triples
+        ; extra the object if subject and subject if object
+        ; go next
+        ; this is a horrible algorithem
+        ; FIXME can use transitive as the accumulator
+        ; FIXME cycles
+        (let ([matches (filter f int-triples)])
+          (cond [(null? matches) matches]
+                [(null? p)
+                 (raise-arguments-error 'predicate-null
+                                        "#:p cannot be null if #:transitive is set"
+                                        "#:p" p
+                                        "#:transitive" transitive)]
+                [(and (null? s) (null? o))
+                 (for/fold ([out matches]
+                            #:result out)
+                           ([s (map (compose gen-value triple-s) matches)]
+                            [o (map (compose gen-value triple-o) matches)])
+                   (append (match-triples #:s o #:p p #:transitive transitive)
+                           (append (match-triples #:p p #:o s #:transitive transitive)
+                                   out)))]
+                [(null? s)  ; search 'backward'
+                 (for/fold ([out matches]
+                            #:result out)
+                           ([s (map (compose gen-value triple-s) matches)])
+                   (append (match-triples #:p p #:o s #:transitive transitive) out))]
+                [(null? o)  ; search 'forward'
+                 (for/fold ([out matches]
+                            #:result out)
+                           ([o (map (compose gen-value triple-o) matches)])
+                   (append (match-triples #:s o #:p p #:transitive transitive) out))]
+                [else (raise-arguments-error 'help! "No idea how we got here.")]))
+        (filter f int-triples)))
+
+  (values add-triple add-triples (λ () int-triples) match-triples))
+
+(module+ test
+  (define store
+    (list
+     (triple (URI "s0") (URI "p0") (URI "o0"))
+     (triple (URI "o0") (URI "p0") (URI "o1"))
+     ))
+  (define-values (add-triple add-triples get-triples match-triples) (instrument-store store))
+  (define out-test (for/fold ([out null] #:result out) ([i '(1 2 3)]) (values (cons i out))))
+  (check-equal? out-test '(3 2 1))
+  (define out-test2 (for/fold ([out null] #:result out) ([i '(1 2 3)]) (cons i out)))
+  (check-equal? out-test2 '(3 2 1))
+  (check-equal? (match-triples #:s "s0" #:p "p0")
+                (list (triple (URI "s0") (URI "p0") (URI "o0"))))
+  (check-equal? (match-triples #:p "p0" #:o "o1")
+                (list (triple (URI "o0") (URI "p0") (URI "o1"))))
+  (check-equal? (sort (match-triples #:s "s0" #:p "p0" #:transitive #t)
+                      string<?
+                      #:key (compose gen-value triple-s))
+                (list (triple (URI "o0") (URI "p0") (URI "o1"))
+                      (triple (URI "s0") (URI "p0") (URI "o0"))))
+  (check-equal? (sort (match-triples #:o "o1" #:p "p0" #:transitive #t)
+                      string<?
+                      #:key (compose gen-value triple-s))
+                (list (triple (URI "o0") (URI "p0") (URI "o1"))
+                      (triple (URI "s0") (URI "p0") (URI "o0")))))
+
+(define-syntax (position stx)
+  (syntax-parse stx
+    [(_ func:id store:id)
+     #'(remove-duplicates (map func store))]))
+
+(define (subjects store)
+  (position triple-s store))
+
+(define (predicates store)
+  (position triple-p store))
+
+(define (objects store)
+  (position triple-o store))
+
+;;; iri helpers
+
+(define-for-syntax (format-or-string-append fstring)
+  (if (string-contains? fstring "~a")
+      #'format
+      #'string-append))
+
+(module+ test
+  (begin-for-syntax
+    (check-equal?
+     (syntax->datum (format-or-string-append "should be string-append"))
+     'string-append)
+    (check-equal? 
+     (syntax->datum (format-or-string-append "should be format ~a"))
+     'format)))
+
+(define-for-syntax (add-~a-suffix fstring-stx)
+  (let ([fstring (syntax-e fstring-stx)])
+    (if (string-contains? fstring "~a")
+        fstring-stx
+        (datum->syntax fstring-stx (string-append fstring "~a")))))
+
+(define-for-syntax (split-fstring fstring-stx)
+  (let ([fstring (syntax-e fstring-stx)])
+    (if (string-contains? fstring "~a")
+        (syntax->list (datum->syntax fstring-stx (string-split fstring "~a" #:trim? #f)
+                                     fstring-stx
+                                     fstring-stx))
+        (list fstring-stx (syntax/loc fstring-stx "")))))
+
+(module+ test
+  (begin-for-syntax
+    (check-equal? (map syntax->datum (split-fstring #'"thing ~a middle"))
+                  '("thing " " middle"))
+    (check-equal? (map syntax->datum (split-fstring #'"thing end~a"))
+                  '("thing end" ""))
+    (check-equal? (map syntax->datum (split-fstring #'"no thing"))
+                  '("no thing" ""))))
+
+(define inverse-lu (make-hash))
+
+(define (qname iri)
+  ; TODO trie
+  ; FIXME longest prefix first.
+  (for/fold ([out #f]  ; for/fold seems like the wrong tool for the job here
+             #:result (if out out iri))
+            ([(pref func) (in-hash (iri-prefixes))]
+             #:when (string-prefix? iri pref)
+             #:final (string-prefix? iri pref))
+    (let* ([maybe-suffix (string-trim iri pref)]
+           [out maybe-suffix]   ; TODO need the suffixes too
+           )
+      (if (non-empty-string? out) (list func out)
+          (list func)))))
+
+(define-syntax (define-id-funcs stx)
+  (syntax-parse stx
+    [(_ [prefix:id (~or* fstring:str function:expr)] ...)
+     #:with (prefix-colon ...) (map (λ (p) (format-id p #:source p "~a:" (syntax-e p)))
+                                    (syntax->list #'(prefix ...)))
+     ;#:with (sfunc ...) (map format-or-string-append (syntax->datum #'(fstring ...)))
+     #:with (normalized-fstring ...) (map add-~a-suffix (syntax->list #'(fstring ...)))
+     #:with ((iri-prefix iri-suffix) ...) (map split-fstring (syntax->list #'(fstring ...)))
+     #'(begin (define (prefix-colon [id ""])
+                (~? function (format normalized-fstring id))) ...
+              ; NOTE we are assuming that all prefixes are unique
+              ; FIXME does this overwrite or no?
+              (hash-union! (iri-prefixes) (hash (~@ iri-prefix 'prefix-colon) ...))
+              )]))
+
+; TODO closed namespaces
+
+(module+ test
+  (define-id-funcs
+    [a "test"]
+    [b "another ~a test"]
+    [c "hello"]
+    )
+  (check-equal? (a: "thing") "testthing")
+  (check-equal? (b: 'another-thing) "another another-thing test")
+  (check-equal? (c: 199291) "hello199291")
+
+  (define-id-funcs
+    [d "hrm"])
+
+  (check-equal? (d: "wat") "hrmwat")
+
+  (check-equal? (qname "hrmhello") '(d: "hello"))
+
+  (qname "test")
+  (a:)
+  (parameterize ([iri-prefixes (make-hash)])
+    (define-id-funcs [d "not-hrm/"])
+    (qname "test")
+    (qname "not-hrm/thing"))
+
+  (gen-short (URI "testsuffix"))
+  )
